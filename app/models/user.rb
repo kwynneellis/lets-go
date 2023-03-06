f648de94991cf5583a57c08d583caf0e45c681df
Kat WynneEllis <k.wynneellis@gmail.com>
2023-03-06 17:19:31 +0000

    validations and associations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :workouts
   has_many :bookings_as_owner, through: :workouts, source: :bookings
+  has_many :user_tags
+  has_many :tags, through: :user_tags
+  has_many :ratings, through: :bookings
+
+  validates :email, :password, :first_name, :last_name, :username, :date_of_birth, presence: true
+  validates :about_me, :fitness_goal, :fitness_level, presence: true
+  validates :email, :username, uniqueness: true
+  validates :fitness_level, numericality: { only_integer: true, in: 1..5 }
+  validates :attendance, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100.0 }
+  validates :avg_rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5.0 }
+  validates :about_me, :fitness_goal, length: { minimum: 50 }
 end
