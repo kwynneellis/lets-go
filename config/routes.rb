a3e983996b7332bb007a603a1ae75f92196741f8
Anthony Deadman <anthony.deadman@outlook.com>
2023-03-07 14:45:37 +0000

    config page changes added resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "workouts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "pages#home"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :workouts, only: %i[index new create show destroy edit update] do
+    resources :bookings, only: %i[new create]
+  end
+  resources :bookings, only: %i[index show destroy edit update]
 end
+
+# Defines the root path route ("/")
+# root "articles#index"
