83b0812e89f0d27fbdd1ab2065bf393901caf74b
Rachel Jones <rachelscjones@gmail.com>
2023-03-07 12:15:37 +0000

    started working on feature; paused to sort out laptop keyboard

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,17 @@
 class WorkoutsController < ApplicationController
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    @workout.user = @user
+    if @workout.save
+      redirect_to workout_path(@workout)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
