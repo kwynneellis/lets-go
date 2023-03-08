6210ea1fa58a575ec8ed9fc56c61fdb0dacae90f
Kat WynneEllis <k.wynneellis@gmail.com>
2023-03-08 12:00:59 +0000

    adds rating resources to routes file, adds new and create method to ratings_controller, adds _form to view/ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,34 @@
 class RatingsController < ApplicationController
+  before_action :set_booking, only: %i[new create]
+
+  def new
+    # We need @restaurant in our `simple_form_for`
+    @booking = Booking.find(params[:booking_id])
+    @rating = Rating.new
+  end
+
+  def create
+    @rating = Rating.new(rating_params)
+    @rating.user = @user
+    @rating.booking = @booking
+    if @rating.save
+      redirect_to bookings_path
+    else
+      render "workouts/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
+  end
+
+  def rating_params
+    params.require(:review).permit(:buddy_attended, :buddy_rating, :comment, :booking_id)
+  end
 end
