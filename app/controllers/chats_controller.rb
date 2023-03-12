d6a7b3c5825c648bd8220372f1a68089e000e674
Rachel Jones <rachelscjones@gmail.com>
2023-03-12 13:46:27 +0000

    created show method and view in chats controller and create method in messages controller

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,14 @@
 class ChatsController < ApplicationController
+  before_action :set_booking, only: %i[show]
+
   def show
     @chat = Chat.find(params[:id])
+    @message = Message.new
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
   end
 end
