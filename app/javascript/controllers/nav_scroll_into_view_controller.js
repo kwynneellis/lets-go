37b3158c229cf8b20bd94179319e7a3ba42af29b
becky lomax <becky.lomax@me.com>
2023-03-13 09:51:30 +0000

    horizontal nav filter category scrolling

--- app/javascript/controllers/nav_scroll_into_view_controller.js
+++ app/javascript/controllers/nav_scroll_into_view_controller.js
@@ -1,35 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="nav-scroll-into-view"
+
 export default class extends Controller {
-  static targets = ["btn"]
+  static targets = ["btn"];
 
   connect() {
     console.log("Hello from nav scroll into view Stimulus controller")
   }
 
   scrollIntoView() {
-    this.elementTarget.scrollIntoView({ behavior: "smooth" })
+    this.btnTarget.addEventListener("click", this.handleClick);
+  }
+
+  handleClick(event) {
+    this.btnTarget.scrollIntoView({ behavior: "smooth", inline: "center" })
   }
 }
-
-
-// // Gave up with data controller data target and tried diff approach - neither working:
-
-// let nav = document.querySelectorAll('nav')
-
-// if (nav) {
-//   let navLinks = document.querySelectorAll('nav a')
-//   let activeLink = document.querySelector('.active')
-//   activeLink.scrollIntoView({ behavior: "smooth", inline: "center" })
-
-//   navLinks.forEach( function(link) {
-//     link.addEventListener("click", (event) => {
-//       navLinks.forEach( function(link) {
-//         link.classList.remove('active')
-//       })
-//       link.classList.add('active')
-//       link.scrollIntoView({ behavior: "smooth", inline: "center" })
-//     })
-//   })
-// }
