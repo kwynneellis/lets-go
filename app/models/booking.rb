464610b81cf1fbd7ac053b4d9f435240d02a8963
Kat WynneEllis <k.wynneellis@gmail.com>
2023-03-14 21:21:29 +0000

    adds reviews to guests profile page, adds some basic styling to reviews.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,7 +6,13 @@ class Booking < ApplicationRecord
 
   after_create :create_chat
 
+  scope :with_ratings, -> { joins(:ratings) }
+
   def self.create_chat
     @chat = Chat.create!(booking_id: booking)
+  end
+
+  def self.ratings_by_others(logged_in_user)
+    Rating.where(id: all.with_ratings.where.not('ratings.user_id' => logged_in_user.id).pluck('ratings.id'))
   end
 end
